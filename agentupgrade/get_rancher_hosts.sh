235655ec780fe53b574c2225d32d661966511c82
niusmallnan <zhangzhibo521@gmail.com>
2017-01-17 17:01:33 +0800

    fix running in crontab

--- agentupgrade/get_rancher_hosts.sh
+++ agentupgrade/get_rancher_hosts.sh
@@ -3,11 +3,13 @@ set -ex
 rm -f all_hosts_ip
 rm -f all_hosts_name
 
-for env in `rancher env -a --format {{.ID}}`; do
-    for host in `rancher --env $env hosts --format json| jq .Host.hostname| sed 's/\"//g'`; do
+RANCHER="/usr/local/bin/rancher"
+
+for env in `$RANCHER env -a --format {{.ID}}`; do
+    for host in `$RANCHER --env $env hosts --format json| jq .Host.hostname| sed 's/\"//g'`; do
         echo "--env ${env} ssh ubuntu@${host}" >> all_hosts_name
     done
-    for host in `rancher --env $env hosts --format json| jq .Host.agentIpAddress| sed 's/\"//g'`; do
+    for host in `$RANCHER --env $env hosts --format json| jq .Host.agentIpAddress| sed 's/\"//g'`; do
         echo "${host}" >> all_hosts_ip
     done
 done
--- agentupgrade/restart_beancounter_agent.sh
+++ agentupgrade/restart_beancounter_agent.sh
@@ -2,14 +2,16 @@
 
 #set -ex
 
-for env in `rancher env -a --format {{.ID}}`; do
+RANCHER="/usr/local/bin/rancher"
+
+for env in `$RANCHER env -a --format {{.ID}}`; do
     bc_service_id=''
-    for d in `rancher --env ${env} ps -s | grep beancounter-agent`; do
+    for d in `$RANCHER --env ${env} ps -s | grep beancounter-agent`; do
         bc_service_id=$d
         break
     done
     if [ ! -z "$bc_service_id"  ]; then
         echo "env: $env, beancounter service: $bc_service_id"
-        rancher restart $bc_service_id
+        $RANCHER restart $bc_service_id
     fi
 done
